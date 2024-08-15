b45508ac4e24b3547da068807b2c1e30d0926ec2
z4nt <antonielf1203@gmail.com>
2024-08-15 20:37:08 -0300

    agora ta 100%

--- src/routes/produtoRoutes.js
+++ src/routes/produtoRoutes.js
@@ -6,7 +6,7 @@ const auth = require('../middlewares/authMiddleware')
 
 router.get('/search', auth.authentication,Controller.list)
 router.get('/:id', auth.authentication,Controller.listId)
-router.post('',Controller.create)
+router.post('', auth.authentication,Controller.create)
 router.put('/:id',auth.authentication,Controller.update)
 router.delete('/:id', auth.authentication,Controller.deletar)
 
